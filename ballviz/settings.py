b9ca521fae2de807e9b6d5c918c7ea05764cc0d8
Matt Chauta <mchauta@nrao.edu>
2018-12-13 14:36:39 -0500

    heroku setup whitenoise

--- ballviz/settings.py
+++ ballviz/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '6*)z5v^$fes5@fqh@n@k0pdruui7oc!$@at=g&(uaapy(r!i2*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
