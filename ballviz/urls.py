23af8faac08865abb116f32cc0128bdcdc028e1e
Matt Chauta <mchauta@nrao.edu>
2018-12-13 15:21:07 -0500

    heroku setup deployment

--- ballviz/urls.py
+++ ballviz/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':settings.STATIC_ROOT)}),
 ]
